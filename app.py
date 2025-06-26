f7572b5364061839bfc07c379da98ca794eef648
Vy Tran <thanhvyttv2112@gmail.com>
2025-06-26 07:47:49 +0700

    commit

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def load_llm():
     MODEL_NAME = "lmsys/vicuna-7b-v1.5"
 
     bnb_config = BitsAndBytesConfig(
-        # load_in_4bit=True,
+        load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=torch.bfloat16,
         bnb_4bit_quant_type="nf4",
